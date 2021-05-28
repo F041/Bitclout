94e630949aa64038ec7770c2f3e5e599b1bc800a
Gabriele Giordano <contact.ggiordano@gmail.com>
2021-05-28 14:49:03 +0200

    Update rugPullPatrol.R

--- rugPullPatrol.R
+++ rugPullPatrol.R
@@ -1,22 +1,11 @@
+### Data Crawling ----
 library("Rcrawler")
 library("rvest")
-library(RCurl)
-
-
-### Data Crawling ----
 data<-Rcrawler(Website = "http://https://cloutgate.com//")
 
 
-	
-link<-
-prova<- read_html("http://cloutgate.com//")  %>%
-  html_nodes("p-0.card-body") %>%
-  html_text()
 
 
-
-
-#main > div > div > div:nth-child(5) > div.p-0.card-body > div.table-responsive > div
 ### Some graphs ---- 
 pairs(data, panel=panel.smooth)
 
